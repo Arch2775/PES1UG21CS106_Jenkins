adca78b55815c32649a9d4c146c3361e8d21de90
Arch2775 <93007037+Arch2775@users.noreply.github.com>
2024-03-09 09:48:52 +0530

    Create hello-2.cpp

---
+++ main/hello-2.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "PES1UG21CS106\n";
+  cout << "Task-5: Pipeline execution\n";
+  return 0;
+}
